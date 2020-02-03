304636a8966ff628d5fbc8159162d51935f6830a
RedCAT <akaineko18@gmail.com>
2020-02-03 21:58:26 +1300

    Updates

---
+++ precache-manifest.c028e38f2f97069388fe8d3e528a06a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a70bd41331326e7565c8413924d206b8",
+    "url": "/movie_app_remake/index.html"
+  },
+  {
+    "revision": "6f5cd96db08bba014007",
+    "url": "/movie_app_remake/static/css/main.efec64a4.chunk.css"
+  },
+  {
+    "revision": "c815d03a7d36cdd903cb",
+    "url": "/movie_app_remake/static/js/2.faa4e53d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/movie_app_remake/static/js/2.faa4e53d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f5cd96db08bba014007",
+    "url": "/movie_app_remake/static/js/main.4bcb572d.chunk.js"
+  },
+  {
+    "revision": "1253e400bef28bb426ff",
+    "url": "/movie_app_remake/static/js/runtime-main.b73f9b2c.js"
+  }
+]);
